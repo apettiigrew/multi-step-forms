1bc7a608e0d351c99071c73143767667bdafed42
Andrew Pettigrew <andrew@theparkplayground.com>
2024-07-09 09:58:28 -0400

    WIP

---
+++ src/app/test/page.js
@@ -0,0 +1,9 @@
+// create page component
+import React from 'react';
+import styles from './styles.module.scss';
+
+export default function TestPage() {
+    return (
+        <p className={styles.p}></p>
+    );
+}
