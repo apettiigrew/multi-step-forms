520b7cea1242a0d27864edd3951ee11af7a5e68e
Andrew Pettigrew <andrew@theparkplayground.com>
2024-07-10 15:41:10 -0400

    Add form content for personal details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,10 @@ import { SubHeading } from "@/components/text/subheading";
 import styles from "./page.module.scss";
 import { StepLabel } from "@/components/form/step-label/step-label";
 import { FourCirleIcon, OneCirleIcon, ThreeCirleIcon, TwoCirleIcon } from "@/components/shared/icons/icons";
+import { InputField } from "@/components/form/input-elements/input-field";
 
 
-export default function CheckoutFormPage() {
+export default function CheckoutPage() {
   return (
     <main className={styles.main}>
       <div className={styles.container}>
@@ -36,7 +37,38 @@ export default function CheckoutFormPage() {
 
             </div>
           </div>
-          <div className={styles["right-content"]}></div>
+          <div className={styles["right-content"]}>
+            <div className={styles["right-content-headings"]}>
+              <SubHeading>Personal Details</SubHeading>
+              <p>Enter your perosnal information to continue to checkout.</p>
+            </div>
+
+            <div>
+              <div className={styles["form-content"]}>
+                <InputField
+                  name="firstName"
+                  label="First Name"
+                  required={true}
+                />
+                <InputField
+                  name="lastName"
+                  label="Last Name"
+                  required={true}
+                />
+                <InputField
+                  name="email"
+                  label="Email Address"
+                  required={true}
+                />
+
+                <InputField
+                  name="phoneNumber"
+                  label="Phone Number"
+                  required={true}
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </main>
--- src/components/form/input-elements/input-field.tsx
+++ src/components/form/input-elements/input-field.tsx
@@ -1,6 +1,8 @@
-import { FieldAttributes, FieldHelperProps, useField } from "formik";
-import { DetailedHTMLProps, InputHTMLAttributes, useCallback } from "react";
-import Select, { ActionMeta } from 'react-select';
+// import { FieldAttributes, FieldHelperProps, useField } from "formik";
+// import { DetailedHTMLProps, InputHTMLAttributes, useCallback } from "react";
+// import Select, { ActionMeta } from 'react-select';
+import { useMemo } from "react";
+import styles from "./input-field.module.scss";
 
 type Props = {
     name: string;
@@ -13,86 +15,113 @@ type InputFieldProps = Props;
 
 export function InputField(props: InputFieldProps) {
     const { name, label, required, ...restOfProps } = props;
-    const [field, meta] = useField(name);
+
+
+    const labelText = useMemo(() => {
+        return required ? `${label}*` : label;
+    }, [label, required]);
 
     return (
         <div className="form-control">
-            <label htmlFor={name}>
-                {label} {required ? "*" : ""}
+            <label htmlFor={name} className={styles.label}>
+                {labelText}
             </label>
-            <input {...field} {...restOfProps} id={name} />
-            {meta.error && meta.touched && (
-                <small className="error">{meta.error}</small>
-            )}
-        </div>
-    );
-};
-
-type CheckBoxProps = Props;
-export const Checkbox = (props: CheckBoxProps) => {
-    const { name, label, ...restOfProps } = props;
-    const [field, meta, helpers] = useField(name);
-
-    return (
-        <div className="form-control">
-            <label className="checkbox-option">
-                <input type="checkbox" {...restOfProps} {...field} />
-                {label}
-            </label>
-            {meta.touched && meta.error && (
-                <small className="error">{meta.error}</small>
-            )}
-        </div>
-    );
-};
-type SelectDropdownProps = Props & {
-    onChange?: (option: SelectOption | null) => SelectOption;
-    options: SelectOption[];
-};
-export function SelectDropdown(props: SelectDropdownProps) {
-    const { name, label, required, options, onChange, ...restOfProps } = props;
-    const [field, meta] = useField(name);
-
-    return (
-        <div className="form-control">
-            <label htmlFor={name}>
-                {label} {required ? "*" : ""}
-            </label>
-            <SelectField
+            <input
+                className={styles['input-field']}
+                id={name}
                 name={name}
-                label={label}
-                required={required}
-                options={options}
-                onChange={onChange}
+                type="text"
+            // onChange={formik.handleChange}
+            // value={formik.values.email}
             />
-            {meta.error && meta.touched && (
-                <small className="error">{meta.error}</small>
-            )}
         </div>
     );
 };
 
-export type SelectOption = { value: string; label: string };
-type SelectFieldProps = Props & {
-    options: SelectOption[];
-    onChange?: (option: SelectOption | null) => SelectOption;
-};
 
-function SelectField(props: SelectFieldProps) {
-    const { options, onChange, ...restOfProps } = props;
 
-    const onHandleChange = useCallback((option: SelectOption | null, actionMeta: ActionMeta<SelectOption>) => {
-        // form.setValue(option ? option.value : "", field.name);
-        if (onChange) {
-            onChange(option);
-        }
-    }, [onChange, options]);
+// type InputFieldProps = Props;
 
-    return (
-        <Select
-            options={options}
-            onChange={onHandleChange}
-        />
-    );
-};
+// export function InputField(props: InputFieldProps) {
+//     const { name, label, required, ...restOfProps } = props;
+//     const [field, meta] = useField(name);
+
+//     return (
+//         <div className="form-control">
+//             <label htmlFor={name}>
+//                 {label} {required ? "*" : ""}
+//             </label>
+//             <input {...field} {...restOfProps} id={name} />
+//             {meta.error && meta.touched && (<small className="error">{meta.error}</small>)}
+//         </div>
+//     );
+// };
+
+// type CheckBoxProps = Props;
+// export const Checkbox = (props: CheckBoxProps) => {
+//     const { name, label, ...restOfProps } = props;
+//     const [field, meta, helpers] = useField(name);
+
+//     return (
+//         <div className="form-control">
+//             <label className="checkbox-option">
+//                 <input type="checkbox" {...restOfProps} {...field} />
+//                 {label}
+//             </label>
+//             {meta.touched && meta.error && (
+//                 <small className="error">{meta.error}</small>
+//             )}
+//         </div>
+//     );
+// };
+// type SelectDropdownProps = Props & {
+//     onChange?: (option: SelectOption | null) => SelectOption;
+//     options: SelectOption[];
+// };
+// export function SelectDropdown(props: SelectDropdownProps) {
+//     const { name, label, required, options, onChange, ...restOfProps } = props;
+//     const [field, meta] = useField(name);
+
+//     return (
+//         <div className="form-control">
+//             <label htmlFor={name}>
+//                 {label} {required ? "*" : ""}
+//             </label>
+//             <SelectField
+//                 name={name}
+//                 label={label}
+//                 required={required}
+//                 options={options}
+//                 onChange={onChange}
+//             />
+//             {meta.error && meta.touched && (
+//                 <small className="error">{meta.error}</small>
+//             )}
+//         </div>
+//     );
+// };
+
+// export type SelectOption = { value: string; label: string };
+// type SelectFieldProps = Props & {
+//     options: SelectOption[];
+//     onChange?: (option: SelectOption | null) => SelectOption;
+// };
+
+// function SelectField(props: SelectFieldProps) {
+//     const { options, onChange, ...restOfProps } = props;
+
+//     const onHandleChange = useCallback((option: SelectOption | null, actionMeta: ActionMeta<SelectOption>) => {
+//         // form.setValue(option ? option.value : "", field.name);
+//         if (onChange) {
+//             onChange(option);
+//         }
+//     }, [onChange, options]);
+
+//     return (
+//         <Select
+//             options={options}
+//             onChange={onHandleChange}
+//         />
+//     );
+// };
 
